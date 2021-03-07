7ade252a12547ec9c1bd085859ded68754f4d98b
mohammadAqel <mohammedaqel100@gmail.com>
2021-03-07 16:34:22 +0200

    save

---
+++ java.js
@@ -0,0 +1,18 @@
+'use strict'; 
+
+
+alert ("WELCOME");
+
+var x=3.32;
+var y=2.23;
+console.log(x*y-y/x);
+
+prompt("please enter the number");
+
+var color=prompt("enter a color")
+if (color == red) {
+    alert("click-right");
+  } else {
+    alert("click-lift");
+  }
+  document.write(color);
